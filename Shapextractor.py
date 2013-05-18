e6e42a10a1f238d708f275e959cbe67e75995c7f
exilaus <exilaus@hotmail.com>
2013-05-18 14:40:11 +0200

    reduce delay and add RESH  RESV  (resolution camera)  and ROT(0 or 90) Rotate image at 90° for vertical mount camera

--- Shapextractor.py
+++ Shapextractor.py
@@ -18,6 +18,9 @@ import ConfigParser
 #DELAY = 0.002 
 #CROPH = 180  #pix to remove from top.(need for  clean image final output)
 #QUALITY = 1  #(0 to 2) 0=512photo  1=2014 2=4028
+RESV=640
+RESH=480
+ROT=0
 
 #PINS = [A,An,B,Bn] #GPIO stepper 
 #SEQA = [(A,),(A,An)]
@@ -29,19 +32,19 @@ import ConfigParser
 #Take Photos and modify ====================================================================================
 def cheese(z):
  i = 0 
- while (i < (640*(480-CROPH)*65/100) or i > (640*(480-CROPH)*90/100) ):
+ while (i < (RESV*(RESH-CROPH)*65/100) or i > (RESV*(RESH-CROPH)*90/100) ):
   im1 = cam.get_image()
-  time.sleep(0.1)     
+  time.sleep(0.05)     
   p.ChangeDutyCycle(12)
-  time.sleep(0.2)
+  time.sleep(0.05)
   im2 = cam.get_image()
-  time.sleep(0.2)
+  time.sleep(0.05)
   p.ChangeDutyCycle(0)
-  time.sleep(0.1)
+  time.sleep(0.05)
   pygame.image.save(im1, "b%08d.jpg" % z)
   pygame.image.save(im2, "a%08d.jpg" % z)
-  im2 = Image.open("b%08d.jpg" % z).crop((0,CROPH,640,480))
-  im1 = Image.open("a%08d.jpg" % z).crop((0,CROPH,640,480))
+  im2 = Image.open("b%08d.jpg" % z).rotate(ROT).crop((0,CROPH,RESV,RESH))
+  im1 = Image.open("a%08d.jpg" % z).rotate(ROT).crop((0,CROPH,RESV,RESH))
   diff = ImageChops.difference(im2, im1)
   diff = ImageOps.grayscale(diff)
   diff = ImageOps.posterize(diff, 6)
@@ -49,7 +52,7 @@ def cheese(z):
   i= v[0][0]
   print i
   im1.save("b%08d.jpg" % z, quality= 90)
-  im1 = Image.new("RGB", (640,480-CROPH))
+  im1 = Image.new("RGB", (RESV,RESH-CROPH))
   im1.paste(diff)
   im1.save("%08d.jpg" % z, quality= 90)
   im2.save("a%08d.jpg" % z, quality= 90)
@@ -100,8 +103,12 @@ gpio.output(LIGHT, gpio.HIGH)
 print 'Init camera....'
 import pygame.image
 pygame.camera.init()
-cam = pygame.camera.Camera(pygame.camera.list_cameras()[0])
+cam = pygame.camera.Camera(pygame.camera.list_cameras()[0],(RESV,RESH))
 cam.start()
+if ROT==90 :
+ z=RESV
+ RESV=RESH
+ RESH=z
 subprocess.call("v4l2-ctl --set-ctrl white_balance_automatic=0" ,shell=True)
 subprocess.call("v4l2-ctl --set-ctrl sharpness=63" ,shell=True)
 
@@ -115,6 +122,8 @@ p.ChangeFrequency(50)
 for x in range(0,512):
  print 'Full step N-' , x
  cheese(z)
+ if z==1 :
+  cheese(z)
  z=z+1
  stepper(SEQA,PINS)
  if QUALITY >>1 :
